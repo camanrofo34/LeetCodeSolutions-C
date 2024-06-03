0cf07ea21f293233234af45c85ff829618d2db2b
CaRf <camanrofo@gmail.com>
2024-06-03 14:51:03 -0500

    Update 4MedianOfTwoSortedArrays.cpp

--- 4MedianOfTwoSortedArrays.cpp
+++ 4MedianOfTwoSortedArrays.cpp
@@ -1,8 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+        int totalSize = nums1.size() + nums2.size();
+        int medianLength = (totalSize / 2) + 1;
+        std::vector<int> sortedNums(medianLength);
+        int iterator1 = 0, iterator2 = 0;
         
+        for (int i = 0; i < medianLength; i++) {
+            if (iterator1 < nums1.size() && (iterator2 >= nums2.size() || nums1[iterator1] <= nums2[iterator2])) {
+                sortedNums[i] = nums1[iterator1];
+                iterator1++;
+            } else {
+                sortedNums[i] = nums2[iterator2];
+                iterator2++;
+            }
+        }
+        
+        if (totalSize % 2 == 1) {
+            return sortedNums[medianLength - 1];
+        } else {
+            return (sortedNums[medianLength - 1] + sortedNums[medianLength - 2]) / 2.0;
+        }
     }
 };
